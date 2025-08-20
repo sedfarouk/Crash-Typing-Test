aa13920330264c9f7c05f76fb0dd22f7f80acfeb
sedfarouk <sadikalhanssah@gmail.com>
2025-08-20 07:11:53 +0000

    docs: add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import './App.css'
 import { ThemeProvider } from './components/theme-provider'
 import HomePage from './pages/HomePage'
 import AppContextProvider from './store/app-store'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
-    <AppContextProvider>
-      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-        <HomePage />
-      </ThemeProvider>
-    </AppContextProvider>
+    <Analytics>
+      <AppContextProvider>
+        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+          <HomePage />
+        </ThemeProvider>
+      </AppContextProvider>
+    </Analytics>
   )
 }
 
