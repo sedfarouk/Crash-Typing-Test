1ddaa9b8b78487e14a32c166406af33606d0b13e
sedfarouk <sadikalhanssah@gmail.com>
2025-08-20 08:53:06 +0000

    analytics import for react

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 import { ThemeProvider } from './components/theme-provider'
 import HomePage from './pages/HomePage'
 import AppContextProvider from './store/app-store'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <AppContextProvider>
       <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
           <HomePage />
-        <Analytics />
+          <Analytics />
       </ThemeProvider>
     </AppContextProvider>
   )
